a057c1efbcf6aa250d44f1814668b370d5d8abb1
VaridhiS <60010568+VaridhiS@users.noreply.github.com>
2026-02-09 20:55:50 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medicalChatbot",
+    version="0.1.0",
+    author="Varidhi",
+    author_email="varidhi.shrivastava@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
